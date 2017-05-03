1c048200d666f19653a1979b324ad2dfc1d08d20
btow <btow@yandex.ru>
2017-05-03 17:25:27 +0500

    It's OK

---
+++ app/src/main/java/com/example/samsung/p1051_fragmentdynamic/Fragment1.java
@@ -0,0 +1,21 @@
+package com.example.samsung.p1051_fragmentdynamic;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by samsung on 03.05.2017.
+ */
+
+public class Fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+}
--- app/src/main/java/com/example/samsung/p1051_fragmentdynamic/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1051_fragmentdynamic/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.samsung.p1051_fragmentdynamic;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    FragmentTransaction fragmentTransaction;
+    CheckBox chbAddToBackStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+
+        chbAddToBackStack = (CheckBox) findViewById(R.id.chbAddToBackStack);
+    }
+
+    public void onClickBtn(View view) {
+
+        fragmentTransaction = getFragmentManager().beginTransaction();
+
+        switch (view.getId()) {
+
+            case R.id.btnAdd :
+                fragmentTransaction.add(R.id.flCont, fragment1);
+                break;
+            case R.id.btnRemove :
+                fragmentTransaction.remove(fragment1);
+                break;
+            case R.id.btnReplace :
+                fragmentTransaction.replace(R.id.flCont, fragment2);
+                break;
+            default:
+                break;
+
+        }
+
+        if (chbAddToBackStack.isChecked()) fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
+
     }
 }
